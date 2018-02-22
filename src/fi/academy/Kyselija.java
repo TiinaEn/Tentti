0d896734c42c3c9303a0afce30e1b0befe5c6c86
hdnurmin <heidi.a.nurminen@gmail.com>
2018-02-22 13:32:32 +0200

    Kyselijan random-metodi luotu

--- src/fi/academy/Kyselija.java
+++ src/fi/academy/Kyselija.java
@@ -1,4 +1,12 @@
 package fi.academy;
 
+import java.util.Random;
+
 public class Kyselija {
+
+    public static int randomHaku() {
+        Random rnd = new Random();
+        int kysymysId = rnd.nextInt(3) +1;
+        return kysymysId;
+    }
 }

ec6fd3131ee581ae4de1e79ea68a5926d8689bf5
TiinaEn <tiina.enroth@outlook.com>
2018-02-22 13:15:24 +0200

    Kysymys-luokkaan tehty ensimmäinen metodi

--- src/fi/academy/Kysymys.java
+++ src/fi/academy/Kysymys.java
@@ -1,4 +1,18 @@
 package fi.academy;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Kysymys {
+
+    public static void annaKysymys (Connection yhteys) throws SQLException {
+        String sql = "select teksti as Kysymys from kysymykset";
+        PreparedStatement prstm = yhteys.prepareStatement(sql);
+        ResultSet rs = prstm.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.getString("Kysymys"));
+        }
+    }
 }

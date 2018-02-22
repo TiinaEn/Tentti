86305548f1735656b413a473c8c726cc0b700b71
HeidiKa <kaunisto.heidi@gmail.com>
2018-02-22 11:44:16 +0200

    Yhteys avattu

--- src/fi/academy/Main.java
+++ src/fi/academy/Main.java
@@ -1,4 +1,13 @@
 package fi.academy;
 
+import java.sql.*;
+
 public class Main {
+    public static void main(String[] args) {
+        try{
+            Yhteys.luoYhteys();
+        }catch (ClassNotFoundException|SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/fi/academy/Yhteys.java
+++ src/fi/academy/Yhteys.java
@@ -1,4 +1,16 @@
 package fi.academy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Yhteys {
+    public static void lataaAjuri() throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("Ajuri ladattu");
+    }
+    public static Connection luoYhteys() throws ClassNotFoundException, SQLException {
+        lataaAjuri();
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/tentti?useSSL=false", "lumiukko", "salasana");
+    }
 }

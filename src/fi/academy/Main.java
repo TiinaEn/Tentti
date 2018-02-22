5f5a9c1806abc32f14ae85e018cf6e7be2f0482d
HeidiKa <kaunisto.heidi@gmail.com>
2018-02-22 13:48:12 +0200

    muutokset mainissa

--- src/fi/academy/Main.java
+++ src/fi/academy/Main.java
@@ -5,6 +5,9 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         try{
+
+            Vaihtoehdot.vastaukset(Yhteys.luoYhteys());
+
             Yhteys.luoYhteys();
         }catch (ClassNotFoundException|SQLException e) {
             System.out.println(e.getMessage());
--- src/fi/academy/Vaihtoehdot.java
+++ src/fi/academy/Vaihtoehdot.java
@@ -1,4 +1,18 @@
 package fi.academy;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Vaihtoehdot {
+
+    public static void vastaukset(Connection yhteys) throws SQLException {
+        String sql = "Select teksti from kysymysvaihtoehdot";
+        PreparedStatement prstm = yhteys.prepareStatement(sql);
+        ResultSet rs = prstm.executeQuery();
+        while (rs.next()) {
+            System.out.println("teksti");
+        }
+    }
 }
